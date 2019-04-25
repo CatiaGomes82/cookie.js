a3484a629789328f882736a1db702ccd9c468f85
CatiaGomes <cgo@codehousegroup.com>
2019-04-25 22:10:41 +0100

    demo - work in progress

--- src/cookie.js
+++ src/cookie.js
@@ -1,61 +1,89 @@
-    window.cookie = new function Client() {
-        var cookieVal = [];
+window.cookie = new function Client() {
+    var cookieVal = [];
 
-        function get(name) {
-            var cookies = document.cookie.replace(/;/g, ''),
-                cookieArray = cookies.split(' ');
+    this.get = function (name) {
+        var cookies = document.cookie.replace(/;/g, ''),
+            cookieArray = cookies.split(' ');
 
-            if (cookies === '') {
-                return '';
-            }
-
-            for (var index = 0; index < cookieArray.length; index += 1) {
-                var cookieStringArray = cookieArray[index].split('=');
-
-                if (typeof name !== 'undefined' && cookieStringArray[0] === name) {
-                    cookieArray = cookieStringArray[1];
-                    break;
-                }
-
-                cookieArray[index] = { name: cookieStringArray[0], value: cookieStringArray[1] };
-            }
-
-            if (typeof name !== 'undefined') {
-                return '';
-            }
-
-            return cookieArray;
+        if (cookies === '') {
+            return '';
         }
 
-        function set(name, value) {
-            cookieVal = [];
+        for (var index = 0; index < cookieArray.length; index += 1) {
+            var cookieStringArray = cookieArray[index].split('=');
 
-            if (typeof name === 'undefined' || typeof value === 'undefined') {
-                return;
+            if (typeof name !== 'undefined' && cookieStringArray[0] === name) {
+                cookieArray = cookieStringArray[1];
+                break;
             }
 
-            cookieVal.push(name + '=' + value + ';');
-            //date.toGMTString()
-            //var date = new Date();
-            //date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
-            cookieVal.push(' expires= Fri, 31 Dec 9999 23:59:59 GMT;');
-
-            cookieVal.push(' path=/;');
-
-          //  cookieVal.push(' domain=example.com;')
-
-           // cookieVal.push('secure');
-            console.log(cookieVal.join(''))
-            document.cookie = cookieVal.join('');
+            cookieArray[index] = { name: cookieStringArray[0], value: cookieStringArray[1] };
         }
 
-        function remove() {
-            //document.cookie = name+'=; Max-Age=-99999999;'; 
+        if (typeof name !== 'undefined') {
+            return '';
         }
 
-        return {
-            get: get,
-            set: set,
-            delete: remove
-        }
+        return cookieArray;
     }
+
+    this.set = function (name, value, opts) {
+        cookieVal = [];
+
+        if (typeof name === 'undefined' || typeof value === 'undefined') {
+            return;
+        }
+
+        cookieVal.push(name + '=' + value + ';');
+
+        if (typeof opts !== 'object') {
+            document.cookie = cookieVal.join('');
+            return;
+        }
+
+        for (var prop in opts) {
+            // not valid property
+            if (!/^(?:expires|max\-age|path|domain|secure)$/i.test(prop)) {
+                return false;
+            }
+
+            switch (prop) {
+                case 'expires':
+                case 'max-age':
+                    var number = parseInt(opts[prop]);
+
+                    if (isNaN(number)) {
+                        cookieVal.push(prop + '=Tue, 19 Jan 2038 00:00:00 GMT;'); // max expiry date
+                    } else {
+                        var date = new Date();
+                        date.setTime(date.getTime() + (number * 24 * 60 * 60 * 1000));
+                        cookieVal.push(prop + '= ' + date.toUTCString() + ';');
+                    }
+
+                    break;
+                case 'domain':
+                    cookieVal.push('domain=' + opts[prop] + ';')
+                    break;
+
+                case 'path':
+                    cookieVal.push(' path=' + opts[prop] + ';');
+                    break;
+
+                case 'secure':
+                    if (typeof opts[prop] === 'boolean') {
+                        cookieVal.push('secure;');
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        console.log(cookieVal.join(''))
+        document.cookie = cookieVal.join('');
+    }
+
+    this.delete = function (name) {
+        document.cookie = name+'=; Max-Age=-99999999;'; 
+    }
+}
